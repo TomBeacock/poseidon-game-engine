b19c5e24b93f92fa5e19e7b02800d4f0b32c603d
Thomas Beacock <thomasbeacock@gmail.com>
2023-01-25 15:39:11 +0000

    Added new constructors

--- src/math/vec3f.rs
+++ src/math/vec3f.rs
@@ -1,5 +1,4 @@
 use core::fmt;
-use std::ops;
 
 use auto_ops::{impl_op_ex, impl_op_ex_commutative};
 
@@ -12,6 +11,10 @@ pub struct Vec3f {
 }
 
 impl Vec3f {
+    pub const fn new(x: f32, y: f32, z: f32) -> Self {
+        Self { x, y, z }
+    }
+
     /// Creates vector: (0, 0, 0) 
     pub const fn zero() -> Self {
         Vec3f { x: 0.0, y: 0.0, z: 0.0 }
